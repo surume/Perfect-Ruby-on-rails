b98ca67c3ef26427a0a3f396e58d7a05f7878c94
Surume <surume0227@gmail.com>
2015-03-15 20:08:21 +0900

    rake db:migrate

--- awesome_eventts/db/migrate/20150315105739_create_events.rb
+++ awesome_eventts/db/migrate/20150315105739_create_events.rb
@@ -2,13 +2,13 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.integer :owner_id
-      t.string :name
-      t.string :place
-      t.datetime :start_time
-      t.string :end_time
-      t.text :content
-
+      t.string :name,         null: false
+      t.string :place,        null: false
+      t.datetime :start_time, null: false
+      t.string :end_time,     null: false
+      t.text :content,        null: false
       t.timestamps null: false
     end
+    add_index :events, :owner_id
   end
 end
