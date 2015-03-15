46dbb4843e59968aff35416c49da8127ec739fb9
Surume <surume0227@gmail.com>
2015-03-15 19:20:48 +0900

    Add gitignore

--- awesome_eventts/app/controllers/sessions_controller.rb
+++ awesome_eventts/app/controllers/sessions_controller.rb
@@ -4,4 +4,9 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     redirect_to root_path, notice: 'ログインしました'
   end
+
+  def destory
+    reset_session
+    redirect_to root_path, notice: 'ログアウトしました'
+  end
 end
