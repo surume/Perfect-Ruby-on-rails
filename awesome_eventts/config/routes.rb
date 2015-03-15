06197019c9dd2fcd9b74ef7212a25deb7abf2570
Surume <surume0227@gmail.com>
2015-03-15 18:37:56 +0900

    Add callback to Route

--- awesome_eventts/config/routes.rb
+++ awesome_eventts/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -7,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get 'welcome/index'
+  get '/auth/:provider/callback' => 'sessions#create'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
