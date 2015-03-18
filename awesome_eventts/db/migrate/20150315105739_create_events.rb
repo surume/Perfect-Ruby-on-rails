3d1cdb22c028993e47c5496a5393434cf08d8c34
Surume <surume0227@gmail.com>
2015-03-18 23:45:35 +0900

    Fix end_time type

--- awesome_eventts/db/migrate/20150315105739_create_events.rb
+++ awesome_eventts/db/migrate/20150315105739_create_events.rb
@@ -5,7 +5,7 @@ class CreateEvents < ActiveRecord::Migration
       t.string :name,         null: false
       t.string :place,        null: false
       t.datetime :start_time, null: false
-      t.string :end_time,     null: false
+      t.datetime :end_time,   null: false
       t.text :content,        null: false
       t.timestamps null: false
     end
