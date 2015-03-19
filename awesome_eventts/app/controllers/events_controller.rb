992aa8ac74ba9cf455b2db5190f1ca1a3f4c5d14
Surume <surume0227@gmail.com>
2015-03-19 23:01:00 +0900

    Add Event update,edit

--- awesome_eventts/app/controllers/events_controller.rb
+++ awesome_eventts/app/controllers/events_controller.rb
@@ -18,6 +18,19 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def edit
+    @event = current_user.created_events.find(params[:id])
+  end
+
+  def update
+    @event = current_user.created_events.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event, notice: '更新しました'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def event_params
